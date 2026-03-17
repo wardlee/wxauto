241e93cdb117b2c33f10301caab6bacd8290f11d
V <VV>
2026-03-17 08:50:46 +0800

    1

--- test_send_message.py
+++ test_send_message.py
@@ -4,7 +4,7 @@ from wxauto import WeChat
 wx = WeChat()
 
 # 发送消息给"马锋"（已注释）
-# wx.SendMsg("你咋现在才到家啊？？？晕！！！", who="马锋")
+wx.SendMsg("你咋现在才到家啊？？？晕！！！", who="马锋")
 
 # 打开与马锋的聊天窗口（已注释）
 # wx.ChatWith("马锋")
@@ -30,31 +30,31 @@ wx = WeChat()
 #     print(f"类型: {msg.type}")
 #     print("-" * 50)
 
-# 监听马锋的消息，有新消息时自动触发
-import time
+# # 监听马锋的消息，有新消息时自动触发
+# import time
 
-print("开始监听马锋的消息，等待新消息...\n")
+# print("开始监听马锋的消息，等待新消息...\n")
 
-# 添加监听对象
-wx.AddListenChat(who="练瓜", savepic=False, savefile=False, savevoice=False)
+# # 添加监听对象
+# wx.AddListenChat(who="练瓜", savepic=False, savefile=False, savevoice=False)
 
-# 持续监听新消息
-try:
-    while True:
-        # 获取监听对象的新消息
-        msgs = wx.GetListenMessage(who="练瓜")
+# # 持续监听新消息
+# try:
+#     while True:
+#         # 获取监听对象的新消息
+#         msgs = wx.GetListenMessage(who="练瓜")
         
-        if msgs:
-            print(f"\n收到新消息！")
-            for msg in msgs:
-                print(f"发送者: {msg.sender}")
-                print(f"内容: {msg.content}")
-                print(f"类型: {msg.type}")
-                print("-" * 50)
+#         if msgs:
+#             print(f"\n收到新消息！")
+#             for msg in msgs:
+#                 print(f"发送者: {msg.sender}")
+#                 print(f"内容: {msg.content}")
+#                 print(f"类型: {msg.type}")
+#                 print("-" * 50)
         
-        # 每隔1秒检查一次
-        time.sleep(1)
+#         # 每隔1秒检查一次
+#         time.sleep(1)
         
-except KeyboardInterrupt:
-    print("\n停止监听")
-    wx.RemoveListenChat(who="马锋")
+# except KeyboardInterrupt:
+#     print("\n停止监听")
+#     wx.RemoveListenChat(who="马锋")
